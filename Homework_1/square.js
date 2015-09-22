9a97c7e334a2b8c53033b8e337d962182614a133
Evan Nichols <evan.t.nichols@gmail.com>
2015-09-22 18:37:53 -0500

    moving chalk, homework tests

--- Homework_1/square.js
+++ Homework_1/square.js
@@ -1,6 +1,38 @@
-function main() 
+square = function(x) 
 {
+	return x*x;
+}
 
+guess = function(n)
+{
+	chalk.print("Enter a number: ");
+
+	return {
+		entrybox : function(str)
+		{
+			var m = parseInt(str);
+			if( m > n )
+			{
+				chalk.println("Too high");
+				return guess(n);
+
+			}
+			else if ( m < n )
+			{
+				chalk.println("Too low");
+				return guess(n);
+			}
+			else
+			{
+				chalk.println("Yay you win!");
+			}
+		}
+	};
+}
+
+main = function() 
+{
+	chalk.println("Bullshit");
 	return guess(4);
 
 	/*var num = prompt("Please enter a number > 4 and < 20: ");
@@ -13,43 +45,10 @@ function main()
 	}
 
 	chalk.println("The square of " + num + " is " + square(num));
-*/
+	*/
 }
 
-function square(x) 
-{
-	return x*x;
-}
 
-guess = function(n)
-{
-	chalk.print("Guess a number: ");
-	return 
-	{
-		entrybox: function(str)
-		
-		{
-			var m = parseInt(str);
-			if(m >n)
-			{
-				chalk.println("Too high");
-				return guess(n);
 
-			}
-			if (m < n)
-			{
-				chalk.println("Too low");
-				return guess(n);
-			}
-			else
-			{
-				chalk.println("Yay you win!");
-			}
-		}
-	}
-}
 
-//write a main that asks user for a size
-//should reprompt for < 4 and > 20
-//don't assume the user entered a number
 
