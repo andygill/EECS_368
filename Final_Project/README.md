35782526ce238f2350328b344f7b0cc8ff328ea2
Evan Nichols <evan.t.nichols@gmail.com>
2015-11-30 19:43:13 -0600

    add Backbone, clean up js

--- Final_Project/README.md
+++ Final_Project/README.md
@@ -1,22 +1,16 @@
 #EECS 368 Final Project
 
-An interactive Haskell implementations of the A* algorithm using Haskell and Javascript.
+An interactive canvas using Backbone.js.
 
 ##Backlog
 
-#####Javascript
+#####Backbone
 - Draw and save nodes for each mouseclick
     - Need Array to Push point pairs to
     - How to implement this?
-- Draw and enter costs for edges between nodes
-- Implement "I'm finished button using" Promises.
-
-#####Haskell
-- Continue taking notes on A* algorithm
-- Look at Haskell implementations
-- Break down data types
 
 #####Completed
+- Implement Backbone.js MVC setup (yay!)
 - Set up index.html
 - Implemented onclick listeners
 - Identify and display x,y coordinates of mouseclick on canvas
--- Final_Project/points.js
+++ Final_Project/points.js
@@ -1,76 +1,71 @@
-// An example Backbone application contributed by
-// [Jérôme Gravel-Niquet](http://jgn.me/). This demo uses a simple
-// [LocalStorage adapter](backbone-localstorage.html)
-// to persist Backbone models within your browser.
+//Backbone Canvas Application
+//A meager attempt to learn Backbone, using Canvas.
 
 // Load the application once the DOM is ready, using `jQuery.ready`:
-//$(function(){
+$(function(){
 
-    // Point Model
-    var Point = Backbone.Model.extend({
+// Point Model
+var Point = Backbone.Model.extend({
 
-        //Define default attributes for the point
-        defaults: {
-                color: 'black',
-                x: 0,
-                w: 0,
-                radius: 6
-            }
-        }); // end Point Model
+defaults: {
+        color: '#FF9000',
+        x: 0,
+        y: 0,
+        radius: 6
+    }
+}); // end Point Model
 
-    //Define our collection of points.
-    var PointSet = Backbone.Collection.extend({
-        model: Point
-    });
+//Collection of Points
+var PointSet = Backbone.Collection.extend({
+    model: Point
+});
 
-    //Canvas View
-    var CanvasView = Backbone.View.extend({
-       render: function() {
-           var model = this.model, ctx = this.options.ctx;
+//A view to display the points on the canvas
+var CanvasView = Backbone.View.extend({
+   render: function() {
+       var model = this.model, ctx = this.options.ctx;
 
-           ctx.fillStyle = 'black';
-           ctx.globalAlpha = 0.1;
-           //draw the circle using the model
-           ctx.arc(model.get("x"), model.get("y"), model.get("radius"),0,2*Math.PI);
-       }
-   }); //end Canvas View
-
-   //SetView
-   var SetView = Backbone.View.extend({
-
-       initialize: function() {
-           this.listenTo(this.collection, "all", this.render);
-       },
-
-       render: function() {
-           var canvas = this.el, ctx = canvas.getContext("2d");
-           ctx.clearRect(0,0,canvas.width, canvas.height);
-
-           this.collection.each(function(model) {
-               var view = new CanvasView({ctx: ctx, model: model});
-               view.render();
-           })
-       }
-
-   }); //end Set View
+       ctx.fillStyle = '#FF9000';
+       ctx.globalAlpha = 0.1;
+       //draw the circle using the model
+       ctx.arc(model.get("x"), model.get("y"), model.get("radius"),0,2*Math.PI);
+       ctx.fill();
+   }
+});
 
 
-   //Instantiate a New PointSet and add a few elements
-   var c = new PointSet();
-   c.add({color: 'black', x: 150, y:150, radius: 6 });
-   c.add({color: 'red', x: 100, y:100, radius: 8 });
+var SetView = Backbone.View.extend({
 
-   //Instantiate a canvas view
-   var v = new CanvasView({
-       el: $("#canvas"),
-       collection: c
-   });
+    //need to add events
 
-   v.render();
+   initialize: function() {
+       this.listenTo(this.collection, "all", this.render);
+   },
+
+   render: function() {
+       var canvas = this.el, ctx = canvas.getContext("2d");
+       ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+       this.collection.each(function(model) {
+           var view = new CanvasView({ctx: ctx, model: model});
+           view.render();
+       })
+   }
+
+}); //end Set View
 
 
+//Instantiate a New PointSet and add a few elements
+var c = new PointSet();
+c.add({color: 'black', x: 150, y:150, radius: 6 });
+c.add({color: 'red', x: 100, y:100, radius: 8 });
 
+//Instantiate a canvas view
+var v = new SetView({
+   el: $("canvas"),
+   collection: c
+});
 
+v.render();
 
-
-//}); // END MAIN FUNCTION
+}); // END MAIN FUNCTION
